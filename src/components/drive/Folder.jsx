79980e5f548a82380bbb9b4a493db9c208f3873b
Hasnain Raza <hasnainraza1072@gmail.com>
2021-07-13 21:24:28 +0530

    reorganised the code a bit

--- src/components/drive/Folder.jsx
+++ src/components/drive/Folder.jsx
@@ -1,43 +1,38 @@
-import { Button } from 'react-bootstrap'
-import React, {useState, useEffect} from 'react'
+import {Button} from 'react-bootstrap'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import {faFolder} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function Folder({folder}) {
 
-  const [isYearFolder, setIsYearFolder] = useState(true) 
+    const [isYearFolder, setIsYearFolder] = useState(true)
 
-  useEffect(() => {
+    useEffect(() => {
 
-     // folder id for years should remain same in the firebase as here!
-    if (
-          folder.id === 'tc0IQ0FQ0gVQelfDVbVh' || 
-          folder.id === 'bwGBb34xtmbhIcO130BE' || 
-          folder.id === 'K8asGgUIDL1kNLUJh8Qp' || 
-          folder.id === 'XvizCjW6t1M0pZt43saZ'
-        ){
-          setIsYearFolder(true)
-        } 
-    else {
-          setIsYearFolder(false)
-          }
-
-  }, [folder.id])
-  
-
-  return (
-
-      <Button variant='transparent' className={'year-card'} as={Link} to={{pathname: `/folders/${folder.id}`, state: {folder}}}>
-        {
-         isYearFolder ?  <img src={`img/years/${folder.name}.svg` } alt={folder.name}/>
-         : <FontAwesomeIcon icon={faFolder}/>
+        // folder id for years should remain same in the firebase as here!
+        if (folder.id === 'tc0IQ0FQ0gVQelfDVbVh' ||
+            folder.id === 'bwGBb34xtmbhIcO130BE' ||
+            folder.id === 'K8asGgUIDL1kNLUJh8Qp' ||
+            folder.id === 'XvizCjW6t1M0pZt43saZ'
+        ) {
+            setIsYearFolder(true)
+        } else {
+            setIsYearFolder(false)
         }
-        {
-          isYearFolder ? <h3>{folder.name}</h3> : <h6>{folder.name}</h6>
-        }
-        
-      </Button>
-          
-  )
+
+    }, [folder.id])
+
+
+    return (
+        <Button variant='transparent' className={'year-card'} as={Link}
+                to={{pathname: `/folders/${folder.id}`, state: {folder}}}>
+            {isYearFolder ?
+                <img src={`img/years/${folder.name}.svg`} alt={folder.name}/>
+                : <FontAwesomeIcon icon={faFolder}/>}
+            {isYearFolder ?
+                <h3>{folder.name}</h3>
+                : <h6>{folder.name}</h6>}
+        </Button>
+    )
 }
--- src/webpages/DashboardPage/DashboardPage.js
+++ src/webpages/DashboardPage/DashboardPage.js
@@ -6,12 +6,12 @@ import {Container} from 'react-bootstrap'
 import {useFolder} from '../../hooks/useFolder'
 import Folder from '../../components/drive/Folder'
 import File from '../../components/drive/File'
-import {useParams, useLocation} from 'react-router-dom'
+import {useLocation, useParams} from 'react-router-dom'
 import AddFileBtn from '../../components/drive/AddFileBtn'
 
 export default function DashboardPage() {
     const {folderId} = useParams()
-    const { state ={} } = useLocation()
+    const {state = {}} = useLocation()
     const {folder, childFolders, childFiles} = useFolder(folderId, state.folder)
 
     return (
@@ -22,33 +22,27 @@ export default function DashboardPage() {
                     <AddFolderBtn currentFolder={folder}/>
                     <AddFileBtn currentFolder={folder}/>
                 </div>
-                {
-                    childFolders.length > 0 && (
-                        <div className={'dashboard-return-wrapper'}>
+                {childFolders.length > 0 ?
+                    <div className={'dashboard-return-wrapper'}>
                         <h1 className={'dashboard-title'}>Welcome to IITI Classroom</h1>
                         <div className={'dashboard-subject-cards'}>
                             {childFolders.map(childFolder => (
-                                    <Folder key={childFolder.id} folder={childFolder}/>
+                                <Folder key={childFolder.id} folder={childFolder}/>
                             ))}
                         </div>
                     </div>
-                        
-                    )
-                }
-                {childFiles.length > 0 && childFolders.length > 0 && <hr/>}
-                {
-                    childFiles.length > 0 && (
-                        <div className="d-flex flex-wrap">
-                            {childFiles.map(childFile => (
-                                <div className='p-2' key={childFile.id}>
-                                    <File file={childFile}/>
-                                </div>
-                            ))}
-                        </div>
-                    )
-                }
+                    : null}
+                {(childFiles.length > 0 && childFolders.length > 0) ? <hr/> : null}
+                {childFiles.length > 0 ?
+                    <div className="d-flex flex-wrap">
+                        {childFiles.map(childFile => (
+                            <div className='p-2' key={childFile.id}>
+                                <File file={childFile}/>
+                            </div>
+                        ))}
+                    </div>
+                    : null}
             </Container>
-       
         </>
     )
 }
