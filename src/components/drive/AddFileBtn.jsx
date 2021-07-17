672669c094546a9518df7c4a9f788b6a131182ca
Suman Jaiswal <sumanj631@gmail.com>
2021-07-17 20:34:40 +0530

    multiple files

--- src/components/drive/AddFileBtn.jsx
+++ src/components/drive/AddFileBtn.jsx
@@ -11,76 +11,82 @@ export default function AddFileBtn({currentFolder}) {
     const [uploadingFiles, setUploadingFiles] = useState([])
 
     function handleUpload(e){
-        const file = e.target.files[0] ? e.target.files[0] : null
-        if(currentFolder===null || file===null ) return 
-        
-        const id = uuidV4()
 
-        setUploadingFiles(previousUploadingFiles => [
-            ...previousUploadingFiles,
-            {id, name: file.name, progress: 0, error: false}
-        ])
+       for (let i = 0; i < e.target.files.length; i++) {
+           
+           
+           const file = e.target.files[i] ? e.target.files[i] : null
+           if(currentFolder===null || file===null ) return 
+           
+           const id = uuidV4()
+    
+           setUploadingFiles(previousUploadingFiles => [
+               ...previousUploadingFiles,
+               {id, name: file.name, progress: 0, error: false}
+           ])
+    
+           const filePath = currentFolder === ROOT_FOLDER ?
+           `${currentFolder.path.join('/')}/${file.name}` :
+           `${currentFolder.path.join('/')}/${currentFolder.name}/${file.name}`
+    
+           const uploadTask = storage
+           .ref(`/files/${filePath}`)
+           .put(file)
+    
+           uploadTask.on('state_changed', snapshot => {
+               const progress = snapshot.bytesTransferred / snapshot.totalBytes
+               setUploadingFiles(previousUploadingFiles => {
+                   return previousUploadingFiles.map(uploadFile => {
+                       if(uploadFile.id === id ) {
+                           return {...uploadFile, progress}
+                       }
+                       return uploadFile
+                   })
+               })
+           }, () => {
+               setUploadingFiles(previousUploadingFiles => {
+                   return previousUploadingFiles.map(uploadFile => {
+                       if(uploadFile.id === id ) {
+                           return {...uploadFile, error: true}
+                       }
+                       return uploadFile
+                   })
+               })
+    
+           }, () => {
+               setUploadingFiles(previousUploadingFiles => {
+                   return previousUploadingFiles.filter(uploadFile => {
+                      return  uploadFile.id !== id
+                   })
+               })
+    
+               uploadTask.snapshot.ref.getDownloadURL().then(url => {
+                 database.files
+                   .where('name', '==', file.name)
+                   .where('folderId', '==', currentFolder.id)
+                   .get()
+                   .then(existingFiles => {
+                       const existingFile = existingFiles[0]
+                       console.log(existingFiles)
+                       if(existingFile){
+                           existingFile.ref.update({url: url})
+                           console.log('updated')
+                       }
+                       else{
+                           database.files.add({
+                              url,
+                              name: file.name,
+                              createdAt: database.timeStamp,
+                              folderId: currentFolder.id
+                           })
+                           console.log('added')
+                       }
+                   })
+                
+               })
+           })
+       }
 
-        const filePath = currentFolder === ROOT_FOLDER ?
-        `${currentFolder.path.join('/')}/${file.name}` :
-        `${currentFolder.path.join('/')}/${currentFolder.name}/${file.name}`
-
-        const uploadTask = storage
-        .ref(`/files/${filePath}`)
-        .put(file)
-
-        uploadTask.on('state_changed', snapshot => {
-            const progress = snapshot.bytesTransferred / snapshot.totalBytes
-            setUploadingFiles(previousUploadingFiles => {
-                return previousUploadingFiles.map(uploadFile => {
-                    if(uploadFile.id === id ) {
-                        return {...uploadFile, progress}
-                    }
-                    return uploadFile
-                })
-            })
-        }, () => {
-            setUploadingFiles(previousUploadingFiles => {
-                return previousUploadingFiles.map(uploadFile => {
-                    if(uploadFile.id === id ) {
-                        return {...uploadFile, error: true}
-                    }
-                    return uploadFile
-                })
-            })
-
-        }, () => {
-            setUploadingFiles(previousUploadingFiles => {
-                return previousUploadingFiles.filter(uploadFile => {
-                   return  uploadFile.id !== id
-                })
-            })
-
-            uploadTask.snapshot.ref.getDownloadURL().then(url => {
-              database.files
-                .where('name', '==', file.name)
-                .where('folderId', '==', currentFolder.id)
-                .get()
-                .then(existingFiles => {
-                    const existingFile = existingFiles[0]
-                    console.log(existingFiles)
-                    if(existingFile){
-                        existingFile.ref.update({url: url})
-                        console.log('updated')
-                    }
-                    else{
-                        database.files.add({
-                           url,
-                           name: file.name,
-                           createdAt: database.timeStamp,
-                           folderId: currentFolder.id
-                        })
-                        console.log('added')
-                    }
-                })
-             
-            })
-        })
 
     }
     return (
@@ -90,7 +96,9 @@ export default function AddFileBtn({currentFolder}) {
                 <input 
                 type="file" 
                 onChange={handleUpload}
-                style={{opacity:'0', position: 'absolute', left:'-10000px'}} />
+                style={{opacity:'0', position: 'absolute', left:'-10000px'}} 
+                multiple
+                />
             </label>
             {
                 uploadingFiles.length > 0 && 
