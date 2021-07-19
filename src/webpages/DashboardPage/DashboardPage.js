a601e81e3f5387ae1c2c136fca41e49144afad25
Hasnain Raza <hasnainraza1072@gmail.com>
2021-07-19 23:12:01 +0530

    fixed the css a bit more

--- src/webpages/DashboardPage/DashboardPage.js
+++ src/webpages/DashboardPage/DashboardPage.js
@@ -4,7 +4,7 @@ import AddFolderBtn from '../../components/drive/AddFolderBtn'
 import FolderBreadcrumbs from '../../components/drive/FolderBreadcrumbs'
 import {Container} from 'react-bootstrap'
 import {useFolder} from '../../hooks/useFolder'
-import Folder from '../../components/drive/Folder'
+import Folder from '../../components/Folder/Folder'
 import File from '../../components/drive/File'
 import {Redirect, useLocation, useParams} from 'react-router-dom'
 import AddFileBtn from '../../components/drive/AddFileBtn'
@@ -18,34 +18,30 @@ export default function DashboardPage(props) {
         return <Redirect to={'/'}/>
     } else {
         return (
-            <>
-                <Container fluid className='mt-2'>
-                    <div className="d-flex align-center">
-                        <FolderBreadcrumbs currentFolder={folder}/>
-                        <AddFolderBtn currentFolder={folder}/>
-                        <AddFileBtn currentFolder={folder}/>
+            <Container fluid className='mt-2'>
+                <div className="d-flex align-center">
+                    <FolderBreadcrumbs currentFolder={folder}/>
+                    <AddFolderBtn currentFolder={folder}/>
+                    <AddFileBtn currentFolder={folder}/>
+                </div>
+                {childFolders.length > 0 ?
+                    <div className={'dashboard-cards-wrapper'}>
+                        {childFolders.map(childFolder => (
+                            <Folder key={childFolder.id} folder={childFolder}/>
+                        ))}
                     </div>
-                    {childFolders.length > 0 ?
-                        <div className={'dashboard-return-wrapper'}>
-                            <div className={'dashboard-subject-cards'}>
-                                {childFolders.map(childFolder => (
-                                    <Folder key={childFolder.id} folder={childFolder}/>
-                                ))}
+                    : null}
+                {(childFiles.length > 0 && childFolders.length > 0) ? <hr/> : null}
+                {childFiles.length > 0 ?
+                    <div className="d-flex flex-wrap files-container">
+                        {childFiles.map(childFile => (
+                            <div className='p-2' key={childFile.id}>
+                                <File file={childFile}/>
                             </div>
-                        </div>
-                        : null}
-                    {(childFiles.length > 0 && childFolders.length > 0) ? <hr/> : null}
-                    {childFiles.length > 0 ?
-                        <div className="d-flex flex-wrap files-container">
-                            {childFiles.map(childFile => (
-                                <div className='p-2' key={childFile.id}>
-                                    <File file={childFile}/>
-                                </div>
-                            ))}
-                        </div>
-                        : null}
-                </Container>
-            </>
+                        ))}
+                    </div>
+                    : null}
+            </Container>
         )
     }
 }
