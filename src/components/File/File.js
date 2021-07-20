67eefa464587dbdbd6f5fbfd03939d485880a675
Suman Jaiswal <sumanj631@gmail.com>
2021-07-20 13:45:45 +0530

    file code clean

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,32 +1,12 @@
-import { faFile } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
+import React from 'react'
 import './File.scss'
 import { Button } from "react-bootstrap";
 
 export default function File({ file }) {
-    // use file.url to show the file in window
-
-    const [hover, setHover] = useState(false)
-
-    // const image =
-    //     <img src={file.url}
-    //         alt={'thumbnail'}
-    //         className={'file-thumbnail'}
-    //     /> // preload the image
-
-    function handleMouseEnter() {
-        setHover(true)
-    }
-
-    function handleMouseLeave() {
-        setHover(false)
-    }
+    // use file.url to show the file in new window
 
     return (
-        <Button variant={'transparent'} className={'file-button outline'}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}>
+        <Button variant={'transparent'} className={'file-button outline'}>
             <a target="_blank" href={file.url} rel="noreferrer">
                 <img src={file.url} alt={file.name}  style={{width: "100px"}} />
             </a>
