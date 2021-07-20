0a925b9af442f579ffb401d0e3113e2d804798fe
Hasnain Raza <hasnainraza1072@gmail.com>
2021-07-20 22:36:43 +0530

    added a file viewer

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './File.scss'
+import {Modal} from "react-bootstrap";
+import FileViewer from 'react-file-viewer';
+import {CustomErrorComponent} from 'custom-error';
 
 export default function File({file}) {
     // use file.url to show the file in new window
 
-    function handleButtonClick(event) {
+    const [open, setOpen] = useState(false)
 
+    function handleButtonClick() {
+        setOpen(true)
+    }
+
+    function closeModal() {
+        setOpen(false)
+    }
+
+    function errorHandler() {
+        console.log('error occurred!')
+        setOpen(false)
     }
 
     return (
-        <button className={'file-button'}
-        onClick={handleButtonClick}>
-            <img src={file.url}
-                 alt={file.name}/>
-            <span>{file.name}</span>
-        </button>
+        <div>
+            <button className={'file-button'}
+                    onClick={handleButtonClick}>
+                <img src={file.url}
+                     alt={file.name}/>
+                <span>{file.name}</span>
+            </button>
+            <Modal show={open}
+                   onHide={closeModal}>
+                <Modal.Body>
+                    <FileViewer
+                        fileType={'png'}
+                        filePath={file.url}
+                        errorComponent={CustomErrorComponent}
+                        onError={errorHandler}/>
+                </Modal.Body>
+            </Modal>
+        </div>
     )
 }
