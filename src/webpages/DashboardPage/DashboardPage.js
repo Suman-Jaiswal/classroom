dac7eef5d6df2885b816c664e510d5a5e6275b1d
Suman Jaiswal <sumanj631@gmail.com>
2021-07-21 18:18:57 +0530

    authentication complete except domain verification

--- src/webpages/DashboardPage/DashboardPage.js
+++ src/webpages/DashboardPage/DashboardPage.js
@@ -2,41 +2,42 @@ import React from 'react'
 import './DashboardPage.scss'
 import AddFolderBtn from '../../components/drive/AddFolderBtn'
 import FolderBreadcrumbs from '../../components/drive/FolderBreadcrumbs'
-import {Container} from 'react-bootstrap'
-import {useFolder} from '../../hooks/useFolder'
+import { Container } from 'react-bootstrap'
+import { useFolder } from '../../hooks/useFolder'
 import Folder from '../../components/Folder/Folder'
 import File from '../../components/File/File'
-import { useLocation, useParams} from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import AddFileBtn from '../../components/drive/AddFileBtn'
 
-export default function DashboardPage(props) {
+export default function DashboardPage({ user }) {
 
-    const {folderId} = useParams()
-    const {state = {}} = useLocation()
-    const {folder, childFolders, childFiles} = useFolder(folderId, state.folder)
+    const { folderId } = useParams()
+    const { state = {} } = useLocation()
+    const { folder, childFolders, childFiles } = useFolder(folderId, state.folder)
 
-        return (
-            <Container fluid className='mt-2'>
-                <div className="d-flex align-center">
-                    <FolderBreadcrumbs currentFolder={folder}/>
-                    <AddFolderBtn currentFolder={folder}/>
-                    <AddFileBtn currentFolder={folder}/>
+    return (
+        <Container fluid className='mt-2'>
+            <div className="d-flex align-center">
+                <FolderBreadcrumbs currentFolder={folder} />
+                <AddFolderBtn currentFolder={folder} />
+                <AddFileBtn currentFolder={folder} />
+            </div>
+            {childFolders.length > 0 ?
+                <div className={'dashboard-cards-wrapper'}>
+                    {childFolders.map(childFolder => (
+                        <Folder key={childFolder.id} folder={childFolder} />
+                    ))}
                 </div>
-                {childFolders.length > 0 ?
-                    <div className={'dashboard-cards-wrapper'}>
-                        {childFolders.map(childFolder => (
-                            <Folder key={childFolder.id} folder={childFolder}/>
-                        ))}
-                    </div>
-                    : null}
-                {(childFiles.length > 0 && childFolders.length > 0) ? <hr/> : null}
-                {childFiles.length > 0 ?
-                    <div className="dashboard-cards-wrapper">
-                        {childFiles.map(childFile => (
-                            <File key={childFile.id} file={childFile}/>
-                        ))}
-                    </div>
-                    : null}
-            </Container>
-        )
+                : null}
+            {(childFiles.length > 0 && childFolders.length > 0) ? <hr /> : null}
+            {childFiles.length > 0 ?
+                <div className="dashboard-cards-wrapper">
+                    {childFiles.map(childFile => (
+                        <File key={childFile.id} file={childFile} />
+                    ))}
+                </div>
+                : null}
+        </Container>
+    )
+
 }
