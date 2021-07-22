07675d8c5df16aa0a12b109c6f2770bb21f828e5
Suman Jaiswal <sumanj631@gmail.com>
2021-07-23 00:03:23 +0530

    .

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './File.scss'
 import DeleteBtn from '../drive/DeleteBtn';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCaretDown, faCaretUp} from "@fortawesome/free-solid-svg-icons";
-import {Modal} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import { Modal } from "react-bootstrap";
 import DownloadBtn from "../drive/DownloadBtn";
 
-export default function File({file}) {
+export default function File({ file }) {
     // use file.url to show the file in new window
     const [open, setOpen] = useState(false)
     const [fileExpanded, setFileExpanded] = useState(false)
@@ -22,33 +22,33 @@ export default function File({file}) {
                 onClick={() => setOpen(!open)}>
 
                 <img src={file.url}
-                     alt={file.name}/>
+                    alt={file.name} />
                 <span>{file.name}</span>
 
             </a>
 
             <div className={'file-options'}
-                 onClick={handleFileOptionsClick}>
+                onClick={handleFileOptionsClick}>
                 <div className={"file-options-buttons-wrapper"}
-                     style={fileExpanded ? {maxHeight: '100px'} : {maxHeight: '0'}}>
-                    <div style={fileExpanded ? {display: 'flex'} : {display: 'none'}}>
-                        <DeleteBtn id={file.id} type='file'/>
-                        <DownloadBtn downloadUrl={file.url} name={file.name}/>
+                    style={fileExpanded ? { maxHeight: '100px' } : { maxHeight: '0' }}>
+                    <div style={fileExpanded ? { display: 'flex' } : { display: 'none' }}>
+                        <DeleteBtn id={file.id} type='file' />
+                        <DownloadBtn downloadUrl={file.url} name={file.name} />
                     </div>
                 </div>
                 <div className={'options-caret-wrapper'}>
                     {fileExpanded ?
                         <FontAwesomeIcon
                             icon={faCaretDown}
-                            onClick={handleFileOptionsClick}/>
+                            onClick={handleFileOptionsClick} />
                         : <FontAwesomeIcon
                             icon={faCaretUp}
-                            onClick={handleFileOptionsClick}/>}
+                            onClick={handleFileOptionsClick} />}
                 </div>
             </div>
 
             <Modal show={open}
-                   onHide={() => setOpen(false)}>
+                onHide={() => setOpen(false)}>
                 <Modal.Body className={'file-preview-modal-body'}>
                     <embed src={file.url}/>
                 </Modal.Body>
