f25de0473663f4f617dda1bde039971be6a2f484
Hasnain Raza <hasnainraza1072@gmail.com>
2021-08-07 12:12:17 +0530

    removed tooltip from logout button and also cleared the code for NavbarComponent.js

--- src/components/NavbarComponent/NavbarComponent.js
+++ src/components/NavbarComponent/NavbarComponent.js
@@ -1,47 +1,28 @@
-import React, {useState} from "react";
+import React from "react";
 import {Link} from 'react-router-dom'
 import "./NavbarComponent.scss";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPowerOff} from "@fortawesome/free-solid-svg-icons";
 import {Button} from "react-bootstrap";
-import ReactTooltip from "react-tooltip";
-
 
 export default function NavbarComponent({signOut, user}) {
 
-    const [tooltipReference, setTooltipReference] = useState(null)
-
     return (
-        <>
-            <nav className={'nav-return-wrapper'}>
-                <Link to={'/'} className={'brand-container'}>
-                    <img id="logo" src={'/img/logo.png'} alt={''}/>
-                    <span>Classroom</span>
-                </Link>
+        <nav className={'nav-return-wrapper'}>
+            <Link to={'/'} className={'brand-container'}>
+                <img id="logo" src={'/img/logo.png'} alt={''}/>
+                <span>Classroom</span>
+            </Link>
 
-                {user ?
-                    <>
-                        <div className={'nav-right-wrapper'}>
-                            <img src={user.photoURL}
-                                 alt={user.name}/>
-                            {user ?
-                                <>
-                                    <p ref={ref => setTooltipReference(ref)}
-                                       data-tip='Log Out'
-                                       data-place={'left'}/>
-                                    <ReactTooltip/>
-                                    <Button variant='outline-danger'
-                                            onClick={signOut}
-                                            onMouseEnter={() => ReactTooltip.show(tooltipReference)}
-                                            onMouseLeave={() => ReactTooltip.hide(tooltipReference)}>
-                                        <FontAwesomeIcon icon={faPowerOff}/>
-                                    </Button>
-                                </>
-                                : null
-                            }
-                        </div>
-                    </>
-                    : null}
-            </nav>
-        </>)
+            {user ?
+                <div className={'nav-right-wrapper'}>
+                    <img src={user.photoURL}
+                         alt={user.name}/>
+                    <Button variant='outline-danger'
+                            onClick={signOut}>
+                        <FontAwesomeIcon icon={faPowerOff}/>
+                    </Button>
+                </div>
+                : null}
+        </nav>)
 }
